55ba2d47ce98006fd2938a69608c61579028eef6
abderrahman rouas <prof.abder@gmail.com>
2024-07-31 22:33:11 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AnimateModule} from 'primeng/animate';
+import { TimelineModule } from 'primeng/timeline';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AnimateModule,
+    TimelineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
